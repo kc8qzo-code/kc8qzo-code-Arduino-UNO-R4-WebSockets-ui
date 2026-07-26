f1f02bd8131b3c1845d9644fcb231b65d2967baa
kc8qzo-code <kc8qzo@gmail.com>
2026-07-25 20:36:16 -0400

    Created new component for creating WebSockets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LatestSensorReadingResponsiveComponent } from './components/latest-sens
 import { SensorReadingComponent } from './components/sensor-reading/sensor-reading.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SensorReadingEditComponent } from './components/sensor-reading/sensor-reading-edit/sensor-reading-edit.component';
+import { SensorReadingCreateComponent } from './components/sensor-reading/sensor-reading-create/sensor-reading-create.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'latest-sensor-reading', component: LatestSensorReadingResponsiveComponent },
   { path: 'sensor-reading', component: SensorReadingComponent },
+  { path: 'sensor-reading/create', component: SensorReadingCreateComponent },
   { path: 'readings/:id', component: SensorReadingEditComponent },
   { path: '**', component: NotFoundComponent }
 ];
