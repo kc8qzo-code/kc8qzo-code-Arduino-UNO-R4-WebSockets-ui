90a5966c769af44f38fd59e9b5e9f8b28ab0ad93
kc8qzo-code <kc8qzo@gmail.com>
2026-07-27 19:48:39 -0400

    Removed regular pulling of readings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LatestSensorReadingResponsiveComponent } from './components/latest-sensor-reading/latest-sensor-reading-responsive/latest-sensor-reading-responsive.component';
-import { SensorReadingComponent } from './components/sensor-reading/sensor-reading.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SensorReadingEditComponent } from './components/sensor-reading/sensor-reading-edit/sensor-reading-edit.component';
 import { SensorReadingCreateComponent } from './components/sensor-reading/sensor-reading-create/sensor-reading-create.component';
@@ -9,7 +8,6 @@ import { SensorReadingCreateComponent } from './components/sensor-reading/sensor
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'latest-sensor-reading', component: LatestSensorReadingResponsiveComponent },
-  { path: 'sensor-reading', component: SensorReadingComponent },
   { path: 'sensor-reading/create', component: SensorReadingCreateComponent },
   { path: 'readings/:id', component: SensorReadingEditComponent },
   { path: '**', component: NotFoundComponent }
